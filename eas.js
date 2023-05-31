d25218186eb78d337cb6abf437abb0f5373012f1
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-05-31 13:18:05 +0300

    Arrange divs to sqare grid

--- eas.js
+++ eas.js
@@ -1,8 +1,16 @@
 const frame = document.querySelector('.frame');
-for (let i = 1; i <= 16; i++) {
+const widthElements = 4;
+const nElements = Math.pow(widthElements, 2);
+const sqSize = 8;
+const maxWidth = widthElements * sqSize;
+
+frame.style.setProperty('max-width', `${maxWidth}em`);
+
+for (let i = 1; i <= nElements; i++) {
     const square = document.createElement('div');
     square.setAttribute('class', 'square');
     square.setAttribute('id', `sq${i}`);
-    square.textContent = '#';
+    square.style.setProperty('--sqSize', `${sqSize}em`);
+    square.textContent = `#${i}`;
     frame.appendChild(square);
 };
