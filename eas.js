5102d80d1e4ea6a9e2070d9653306efc01c16396
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-06-01 19:23:48 +0300

    Complete basic functionality

--- eas.js
+++ eas.js
@@ -1,28 +1,44 @@
-const frame = document.querySelector('.frame');
-// const ratio = 3;
-// const widthElements = 100 / ratio;
-const widthElements = 50;
-const nElements = Math.pow(widthElements, 2);
-// const sqSize = Math.round(((widthElements * 0.003555) * Math.pow(ratio, 2)) * 10000) / 10000;
-const maxWidth = 768;
-const sqSize = maxWidth / widthElements;
-// const maxWidth = widthElements * sqSize;
+let squaresPerSide = 16;
+const canvas = document.querySelector('.canvas');
 
-frame.style.setProperty('max-width', `${maxWidth}px`);
+function drawCanvas(squaresPerSide) {
+    const resolution = squaresPerSide;
+    const nElements = Math.pow(resolution, 2);
+    const maxWidth = 640;
+    const sqSize = maxWidth / resolution;
 
-for (let i = 1; i <= nElements; i++) {
-    const square = document.createElement('div');
-    square.setAttribute('class', 'square');
-    square.setAttribute('id', `sq${i}`);
-    square.style.setProperty('--sqSize', `${sqSize}px`);
-    // square.textContent = `#${i}`;
-    frame.appendChild(square);
-};
+    canvas.style.setProperty('max-width', `${maxWidth}px`);
 
-const pixels = document.querySelectorAll('.square');
+    for (let i = 1; i <= nElements; i++) {
+        const square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        square.setAttribute('id', `sq${i}`);
+        square.style.setProperty('--sqSize', `${sqSize}px`);
+        // square.textContent = `#${i}`;
+        canvas.appendChild(square);
+    };
 
-function changeColor(pixel) {
-    pixel.addEventListener('mouseenter', e => e.target.style.setProperty('background-color', 'black'));
+    const pixels = document.querySelectorAll('.square');
+
+    function changeColor(pixel) {
+        pixel.addEventListener('mouseenter', e => e.target.style.setProperty('background-color', 'black'));
+    }
+
+    pixels.forEach(e => changeColor(e));
+}
+function removeCanvas() {
+    while (canvas.childElementCount > 0) {
+        canvas.lastChild.remove();
+    };
 }
 
-pixels.forEach(e => changeColor(e));
+drawCanvas(squaresPerSide);
+
+const setCanvasButton = document.querySelector('#set-canvas');
+setCanvasButton.addEventListener('click', () => {
+    squaresPerSide = +prompt('Number of squares per side (from 16 to 100):', 64);
+    if (16 <= squaresPerSide && squaresPerSide <= 100) {
+        removeCanvas();
+        drawCanvas(squaresPerSide);
+    } else alert('Number of squares shold be between 16 and 100.');
+});
