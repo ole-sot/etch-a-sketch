685888f5eb8376c3ea1fcabf76c81f0a1d121cc7
Oleg Sotnikov <127748313+ole-sot@users.noreply.github.com>
2023-06-03 22:09:22 +0300

    Blackier every second

--- eas.js
+++ eas.js
@@ -16,17 +16,20 @@ function drawCanvas(squaresPerSide) {
         square.style.setProperty('--sqSize', `${sqSize}px`);
         canvas.appendChild(square);
     };
-
+}
+function startDrawing(alpha) {
     const pixels = document.querySelectorAll('.square');
-
+    let color = `rgb(0 0 0 / ${alpha})`;
+    
     function changeColor(pixel) {
         pixel.addEventListener('mouseenter', e => {
-            e.target.style.setProperty('background-color', 'black');
+            e.target.style.setProperty('background-color', color);
         });
     }
-
+    
     pixels.forEach(pixel => changeColor(pixel));
 }
+
 function removeCanvas() {
     while (canvas.childElementCount > 0) {
         canvas.lastChild.remove();
@@ -35,11 +38,26 @@ function removeCanvas() {
 
 drawCanvas(squaresPerSide);
 
+function waitforme(millisec) {
+    return new Promise(resolve => {
+        setTimeout(() => { resolve('') }, millisec);
+    });
+}
+async function aDrawing() {
+    for (let alpha = 1; alpha <= 10; alpha++) {
+        await waitforme(1000);
+        startDrawing(alpha / 10);
+    };
+}
+
+aDrawing();
+
 const setCanvasButton = document.querySelector('#set-canvas');
 setCanvasButton.addEventListener('click', () => {
     squaresPerSide = +prompt('Number of squares per side (from 16 to 100):', 64);
     if (16 <= squaresPerSide && squaresPerSide <= 100) {
         removeCanvas();
         drawCanvas(squaresPerSide);
+        aDrawing();
     } else alert('Number of squares shold be between 16 and 100.');
 });
